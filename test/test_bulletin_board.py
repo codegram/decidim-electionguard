9e0089c9c61b81d67ad64236fb55a73632921300
Leonardo Diez <leiodd@gmail.com>
2020-10-02 18:45:14 +0200

    style: normalize use of quotes

--- test/test_bulletin_board.py
+++ test/test_bulletin_board.py
@@ -3,22 +3,22 @@ from .utils import create_election_test_message, trustees_public_keys
 from decidim.electionguard.bulletin_board import BulletinBoard
 
 
-class TestTrustee(unittest.TestCase):
+class TestBulletinBoard(unittest.TestCase):
     def setUp(self):
         self.bulletin_board = BulletinBoard()
 
     def test_key_ceremony(self):
         election_message = create_election_test_message()
-        self.bulletin_board.process_message("create_election", election_message)
+        self.bulletin_board.process_message('create_election', election_message)
 
         for public_keys in trustees_public_keys():
-            self.bulletin_board.process_message("trustee_election_keys", public_keys)
+            self.bulletin_board.process_message('trustee_election_keys', public_keys)
 
         for trustee in election_message['trustees']:
-            self.bulletin_board.process_message("trustee_election_partial_key_backup", {'owner_id': trustee['name']})
+            self.bulletin_board.process_message('trustee_partial_election_key', [{'owner_id': trustee['name']}])
 
         for trustee in election_message['trustees']:
-            joint_key = self.bulletin_board.process_message("trustee_verification", {'owner_id': trustee['name']})
+            joint_key = self.bulletin_board.process_message('trustee_verification', [{'verifier_id': trustee['name']}])
 
         print(joint_key)
 
--- test/test_trustee.py
+++ test/test_trustee.py
@@ -7,56 +7,56 @@ from .utils import create_election_test_message
 
 class TestTrustee(unittest.TestCase):
     def setUp(self):
-        self.trustees = [Trustee("alicia"), Trustee("bob"), Trustee("clara")]
+        self.trustees = [Trustee('alicia'), Trustee('bob'), Trustee('clara')]
 
     def test_key_ceremony(self):
         trustees_public_keys = [
-            trustee.process_message("create_election", create_election_test_message())
+            trustee.process_message('create_election', create_election_test_message())
             for trustee in self.trustees
         ]
 
-        print("\n---- PUBLIC KEYS ----")
+        print('\n---- PUBLIC KEYS ----')
         print(repr(trustees_public_keys))
-        print("---- END PUBLIC KEYS ----\n")
+        print('---- END PUBLIC KEYS ----\n')
 
         trustees_partial_public_keys = list(filter(None, [
-            trustee.process_message("trustee_election_keys", public_keys)
+            trustee.process_message('trustee_election_keys', public_keys)
             for public_keys in trustees_public_keys
             for trustee in self.trustees
             if trustee.context.guardian_id != public_keys['owner_id']
         ]))
 
-        print("\n---- PARTIAL PUBLIC KEYS ----")
+        print('\n---- PARTIAL PUBLIC KEYS ----')
         print(repr(trustees_partial_public_keys))
-        print("---- END PARTIAL PUBLIC KEYS ----\n")
+        print('---- END PARTIAL PUBLIC KEYS ----\n')
 
         trustees_verifications = list(filter(None, [
-            trustee.process_message("trustee_partial_election_key", partial_public_keys)
+            trustee.process_message('trustee_partial_election_key', partial_public_keys)
             for partial_public_keys in trustees_partial_public_keys
             for trustee in self.trustees
             if trustee.context.guardian_id != partial_public_keys[0]['owner_id']
         ]))
 
-        print("\n---- VERIFICATIONS ----")
+        print('\n---- VERIFICATIONS ----')
         print(repr(trustees_verifications))
-        print("---- END VERIFICATIONS ----\n")
+        print('---- END VERIFICATIONS ----\n')
 
         # Process verifications results
         for verification in trustees_verifications:
             for trustee in self.trustees:
                 if trustee.context.guardian_id != verification[0]['verifier_id']:
-                    trustee.process_message("verify_trustee", verification)
+                    trustee.process_message('trustee_verification', verification)
 
         # Simulate the message from the Bulletin Board
-        joint_election_key = serialize(
-            elgamal_combine_public_keys(
+        joint_election_key = serialize({
+            'joint_election_key': elgamal_combine_public_keys(
                 deserialize_key(public_keys['election_public_key'])
                 for public_keys in trustees_public_keys
             )
-        )
+        })
 
         for trustee in self.trustees:
-            print(repr(trustee.process_message("joint_election_key", joint_election_key)))
+            print(repr(trustee.process_message('joint_election_key', joint_election_key)))
 
         # TODO: assert ballot keys
         # TODO: assert ballot constests keys
