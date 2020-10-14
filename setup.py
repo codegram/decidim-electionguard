d5a08bbb11eb7adddc386356bcd9d50c73cd7e71
Leonardo Diez <leiodd@gmail.com>
2020-10-14 15:38:56 +0200

    fix: change package name to be loaded from pyodide

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='decidim.electionguard',
+setup(name='decidim-electionguard',
       version='0.1.0',
       description='ElectionGuard wrappers to be used by Decidim eVoting project',
       url='http://github.com/codegram/decidim.electionguard',
